047f75ba442ac7366af4a6e8c0bc512d25dc5317
sureshpasam <sureshpasam94@gmail.com>
2023-08-25 16:56:04 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sureshpasamawsdevops/simple-python-flask-app:latestSuresh
+docker pull sureshpasamawsdevops/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sureshpasamawsdevops/simple-python-flask-app:latestSuresh
+docker run -d -p 5000:5000 sureshpasamawsdevops/simple-python-flask-app
