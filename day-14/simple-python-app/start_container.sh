b2feadce7417c2c24bbf48461a4ebbdcfd5bf1c7
sureshpasam <sureshpasam94@gmail.com>
2024-06-20 08:23:18 +0530

    start_container changes

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "pulling the docker image from the docker repo"
+
+docker pull sureshpasamawsdevops/simple-python-flask-app:latestAPP
 
 # Run the Docker image as a container
-echo
+echo "Run the docker image for 5000 port"
+docker run -d -p 5000:5000
