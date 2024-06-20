a8c89116e33da64690a26084c5025b44b7573f0f
sureshpasam <sureshpasam94@gmail.com>
2024-06-20 08:27:57 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+!/bin/bash
 set -e
+ids=$(docker ps -aq)
+for id in $ids
+do
+  echo "$id"
+  docker stop $id && docker rm -f $id
+done
 
 # Stop the running container (if any)
-echo "Hi"
+#echo "Hi docker stopped"
+#containerId='docker ps | awk -f ""'{print$1}''
+#docker rm -f $containerId
